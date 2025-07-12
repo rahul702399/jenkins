3c8fd4562ce1b7718a600d57b8d0e20ec794f920
root <root@192.168.1.21>
2025-07-12 14:37:28 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/info")
+def lwinfo():
+    return "I am Harsh from India"
+
+@app.route("/phone")
+def lwphone():
+    return "4564134596"
+
+# Only run the server if this file is executed directly
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
---
+++ test.py
@@ -0,0 +1,4 @@
+from app import lwphone
+
+def test_lwphone():
+	assert lwphone() == "4564134596"
